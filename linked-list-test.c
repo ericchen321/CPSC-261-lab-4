f30dbe977c21da1982e1a115fb7fbbe257936cc2
eric <cgx1997@outlook.com>
2019-02-05 02:19:28 -0800

    set up testing framework

--- linked-list-test.c
+++ linked-list-test.c
@@ -10,29 +10,45 @@ typedef int bool;
 #define CONTENTS_1_LENGTH 1
 #define SUM 
 
-int main(int argc, char *argv[]) {
-  
+/*
+ * effects: initialize 3 test lists:
+ *          list_0: 2 4 -17 13 -6 5 8
+ *          list_1: 99
+ *          list_2: empty
+ */
+void initialize_test_lists(s_list **list_0, s_list **list_1, s_list **list_2){
   int i;
   long contents_0[CONTENTS_0_LENGTH] = { 8, 5, -6, 13, -17, 4, 2 };
   long contents_1[CONTENTS_1_LENGTH] = {99};
   
-  struct s_list *test_list_0 = create_list();
+  *list_0 = create_list();
   for (i = 0; i <CONTENTS_0_LENGTH; i++) {
-    insert_front(test_list_0, contents_0[i]);
+    insert_front(*list_0, contents_0[i]);
   }
 
-  struct s_list *test_list_1 = create_list();
+  *list_1 = create_list();
   for (i = 0; i <CONTENTS_1_LENGTH; i++) {
-    insert_front(test_list_1, contents_1[i]);
+    insert_front(*list_1, contents_1[i]);
   }
   
-  struct s_list *test_list_2 = create_list();
+  *list_2 = create_list();
+}
+
+int main(int argc, char *argv[]) {
+  struct s_list* test_list_0;
+  struct s_list* test_list_1;
+  struct s_list* test_list_2;    
 
   // tests: print_with_positions
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
   print_with_positions(test_list_0);
   print_with_positions(test_list_1);
+  free_list(test_list_0);
+  free_list(test_list_1);
+  free_list(test_list_2);
   
   // tests: get_element_at
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
   if(get_element_at(test_list_0, 5) != 5)
     printf("get_element_at(test_list_0,5) failed");
   if(get_element_at(test_list_0, 2) != -17)
@@ -45,7 +61,12 @@ int main(int argc, char *argv[]) {
     printf("get_element_at(test_list_1, 0) failed");
   if(get_element_at(test_list_2, 0) != LONG_MIN)
     printf("get_element_at(test_list_2, 0) failed");
-    
   free_list(test_list_0);
+  free_list(test_list_1);
+  free_list(test_list_2);
+
+  // tests: insert_at
+
+  
   return 0;
 }
