2459a25132a94a6e79ac128b4fe174ecfc9e74d5
eric <cgx1997@outlook.com>
2019-02-06 00:39:03 -0800

    added 3 test lists

--- linked-list-test.c
+++ linked-list-test.c
@@ -8,6 +8,8 @@ typedef int bool;
 
 #define CONTENTS_0_LENGTH 7
 #define CONTENTS_1_LENGTH 1
+#define CONTENTS_3_LENGTH 1
+#define CONTENTS_5_LENGTH 3
 #define SUM 
 
 /*
@@ -15,11 +17,16 @@ typedef int bool;
  *          list_0: 2 4 -17 13 -6 5 8
  *          list_1: 99
  *          list_2: empty
+ *          list_3: 88
+ *          list_4: empty
+ *          list_5: 33 500 -52
  */
-void initialize_test_lists(s_list **list_0, s_list **list_1, s_list **list_2){
+void initialize_test_lists(s_list **list_0, s_list **list_1, s_list **list_2, s_list **list_3, s_list **list_4, s_list **list_5){
   int i;
   long contents_0[CONTENTS_0_LENGTH] = { 8, 5, -6, 13, -17, 4, 2 };
   long contents_1[CONTENTS_1_LENGTH] = {99};
+  long contents_3[CONTENTS_3_LENGTH] = {88};
+  long contents_5[CONTENTS_5_LENGTH] = {-52, 500, 33};
   
   *list_0 = create_list();
   for (i = 0; i <CONTENTS_0_LENGTH; i++) {
@@ -32,22 +39,37 @@ void initialize_test_lists(s_list **list_0, s_list **list_1, s_list **list_2){
   }
   
   *list_2 = create_list();
+
+  *list_3 = create_list();
+  for (i = 0; i <CONTENTS_3_LENGTH; i++) {
+    insert_front(*list_3, contents_3[i]);
+  }
+
+  *list_4 = create_list();
+
+  *list_5 = create_list();
+  for (i = 0; i <CONTENTS_5_LENGTH; i++) {
+    insert_front(*list_5, contents_5[i]);
+  }
 }
 
 int main(int argc, char *argv[]) {
   struct s_list* test_list_0;
   struct s_list* test_list_1;
-  struct s_list* test_list_2;    
+  struct s_list* test_list_2;
+  struct s_list* test_list_3;
+  struct s_list* test_list_4;
+  struct s_list* test_list_5;    
 
   // tests: print_with_positions
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   print_with_positions(test_list_0);
   print_with_positions(test_list_1);
   print_with_positions(test_list_2);
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
   
   // tests: get_element_at
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   if(get_element_at(test_list_0, 5) != 5)
     printf("get_element_at(test_list_0,5) failed\n");
   if(get_element_at(test_list_0, 2) != -17)
@@ -60,35 +82,35 @@ int main(int argc, char *argv[]) {
     printf("get_element_at(test_list_1, 0) failed\n");
   if(get_element_at(test_list_2, 0) != LONG_MIN)
     printf("get_element_at(test_list_2, 0) failed\n");
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
   // tests: get_sum_of_elements
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: sum of list 0 (non-empty list)
   if(get_sum_of_elements(test_list_0) == 9){}
   else{
     printf("get sum of list 0 failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: sum of list 1 (one-element list)
   if(get_sum_of_elements(test_list_1) == 99){}
   else{
     printf("get sum of list 1 failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: sum of empty list
   if(get_sum_of_elements(test_list_2) == 0){}
   else{
     printf("get sum of empty list failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
   // tests: insert_at
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos 0 in the empty list
   if(insert_at(test_list_2, 0, 33) 
       && get_element_at(test_list_2, 0) == 33
@@ -96,9 +118,9 @@ int main(int argc, char *argv[]) {
   else{
     printf("insert 33 to empty list failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos 0 in list 0 (non-empty list)
   if(insert_at(test_list_0, 0, 161)
       && get_element_at(test_list_0, 0) == 161
@@ -113,9 +135,9 @@ int main(int argc, char *argv[]) {
   else{
     printf("insert 161 to list 0 failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: add a new node to the end of list 0 (non-empty list)
   if( insert_at(test_list_0, 7, 9999)
       && get_element_at(test_list_0, 0) == 2
@@ -131,18 +153,18 @@ int main(int argc, char *argv[]) {
   {
     printf("insert 9999 to end of list 0 failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos 1 in the empty list
   if(!insert_at(test_list_2, 4, 11)
       && test_list_2->num_elements == 0){}
   else{
     printf("empty list overflow insert test failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos 8 in list 0
   if(!insert_at(test_list_0, 8, 444)
       && test_list_0->num_elements == 7
@@ -150,27 +172,27 @@ int main(int argc, char *argv[]) {
   else{
     printf("list 0 overflow insert test failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos -1 in the empty lsit
   if(!insert_at(test_list_2, -1, 222)
     && test_list_2->num_elements == 0){}
   else{
     printf("empty list underflow insert test failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at pos -1 in list 1
   if(!insert_at(test_list_1, -1, 355)
       && test_list_1->num_elements == 1){}
   else{
     printf("one element underflow insert test failed\n");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at  pos 3 in list 0
   if(insert_at(test_list_0, 3, 555)
       && get_element_at(test_list_0, 0) == 2
@@ -186,9 +208,9 @@ int main(int argc, char *argv[]) {
   {
     printf("insert 555 to pos 3 in list 0 failed");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
 
-  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2);
+  initialize_test_lists(&test_list_0, &test_list_1, &test_list_2, &test_list_3, &test_list_4, &test_list_5);
   // case: insert new node at  pos 6 in list 0
   if(insert_at(test_list_0, 6, 3721)
       && get_element_at(test_list_0, 0) == 2
@@ -204,7 +226,10 @@ int main(int argc, char *argv[]) {
   {
     printf("insert 3721 to pos 6 in list 0 failed");
   }
-  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2);
+  free_list(test_list_0); free_list(test_list_1); free_list(test_list_2); free_list(test_list_3); free_list(test_list_4); free_list(test_list_5);
+
+  // tests: insert_list_at
+
   
   return 0;
 }
